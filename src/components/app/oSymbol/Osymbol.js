a3451f355aff1af799074d049e6b635a8d6fd996
Christina Mills <chrimills@gmail.com>
2018-03-09 10:12:03 -0800

    symbols addding

---
+++ src/components/app/oSymbol/Osymbol.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Symbol } from './BlankSymbol';
+
+const OSymbol = (props) => {
+  return (
+    <Symbol className={`symbol column${props.postiiton}`}>
+      <svg viewBox=" 0 0 56 56">
+        <circle cx={28} cy={28} r={25} stroke="black" strokeWidth="2" fill="none" />
+      </svg>  
+    </Symbol>
+  );
+};
+
+export default OSymbol;
---
+++ src/components/app/xSymbol/XSymbol.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Symbol } from './BlankSymbol';
+
+const XSymbol = (props) => {
+  return (
+    <Symbol className={`symbol column${props.postiiton}`}>
+      <svg viewBox=" 0 0 56 56">
+        <line x1="2" y1="2" x2="54" y2='54' stroke="black" strokeWidth="2"/>
+        <
+      </svg>  
+    </Symbol>
+  );
+};
+
+export default OSymbol;
