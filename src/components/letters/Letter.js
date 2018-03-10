a6a1731a55c550cd2dd479b92cffe22324760dc0
G.Provost <grace.g.provost@gmail.com>
2018-03-09 16:10:27 -0800

    make letter selection button disabled after selection

--- src/components/letters/Letter.js
+++ src/components/letters/Letter.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Letter extends Component {
+
+class Letter extends Component {
+
+  state = {
+    selected: false
+  };
+
+  componentWillReceiveProps(nextProps) {
+    const { guesses, letter } = this.props;
+    if(nextProps.guesses.includes(letter)) this.setState({ selected: true });
+  }
 
   render() {
 
     const { letter, onSelect } = this.props;
+    const { selected } = this.state;
 
     return (
-      <button className="letter" onClick={(event) => onSelect(event.target)} value={letter}>
-        {letter}
-      </button>
+        <button className="letter" onClick={(event) => onSelect(event.target)} value={letter} disabled={selected ? true : false}>
+          {letter}
+        </button>
+
     );
   }
 }
+
+export default connect(
+  state => ({ guesses: state.guesses }),
+  null
+)(Letter);
