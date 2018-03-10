4225463aaedb1736a2da4eccac77cc3482c3dba2
ivan limongan <ivan.limongan@gmail.com>
2018-03-09 21:39:57 -0800

    pulling state upward

--- src/components/app/board/Board.js
+++ src/components/app/board/Board.js
@@ -5,8 +5,15 @@ import Square from '../square/Square';
 
 class Board extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
