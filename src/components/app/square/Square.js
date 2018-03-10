d28923cb8121f21a784e98e01a294ec46c279ca1
ivan limongan <ivan.limongan@gmail.com>
2018-03-09 21:28:52 -0800

    added onclick value to X when click tile square

--- src/components/app/square/Square.js
+++ src/components/app/square/Square.js
@@ -4,10 +4,18 @@ import '../App.css';
 
 class Square extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
 
     return (
-      <button className="square">
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
         {this.props.value}
       </button>
     );
